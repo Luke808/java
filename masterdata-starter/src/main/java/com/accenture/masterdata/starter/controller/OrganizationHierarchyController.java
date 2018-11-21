427074204f258ae2f22d313aca239d62a5542397
fujie.wan <fujie.wan@accenture.com>
2018-11-21 18:08:25 +0800

    Organization

--- masterdata-starter/src/main/java/com/accenture/masterdata/starter/controller/OrganizationHierarchyController.java
+++ masterdata-starter/src/main/java/com/accenture/masterdata/starter/controller/OrganizationHierarchyController.java
@@ -21,6 +21,11 @@ import com.accenture.masterdata.organization.service.OrganizationHierarchyServic
 import com.accenture.smsf.framework.starter.web.core.annotation.RestController;
 import com.google.common.collect.Maps;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Api(value="Hierarchy Controller",tags={"组织层级接口"})
 @RestController
 @Validated
 @RequestMapping("/masterdata/orgHierarchy")
@@ -28,16 +33,18 @@ public class OrganizationHierarchyController{
 	@Autowired
 	OrganizationHierarchyService hierarchy;
 
+	@ApiOperation(value="通过层级ID,获取单条层级信息",notes="注意： 参数ID为必须项")
 	@GetMapping("/get")
-	public Map<String, Object> get(@RequestParam("id") Long id) {
+	public Map<String, Object> get(@ApiParam(name="id",value="组织层级ID",required=true) @RequestParam("id") Long id) {
 		OrganizationHierarchy hierarchies = hierarchy.selectOrganizationHierarchy(id);
 		Map<String, Object> result = Maps.newHashMap();
 		result.put("data", hierarchies);
 		return result;
 	}
-	
+
+	@ApiOperation(value="根据通用组合查询条件,获取符合条件的层级信息",notes="注意： 参数param为必须项")
 	@PostMapping("/getList")
-	public Map<String, Object> getList(@RequestBody QueryParam param) {
+	public Map<String, Object> getList(@ApiParam(name="param",value="通用查询条件对象",required=true) @RequestBody QueryParam param) {
 		Map<String, Object> result = Maps.newHashMap();
 		int count = hierarchy.selectOrganizationHierCount(param);
 		List<OrganizationHierarchy> hierarchies = hierarchy.selectOrganizationHierarchys(param);
@@ -48,6 +55,7 @@ public class OrganizationHierarchyController{
 		return result;
 	}
 	
+	@ApiOperation(value="获取层级下拉列表",notes="注意： 无需参数")
 	@GetMapping("/getDropDown")
 	public Map<String, Object> getDropDown() {
 		Map<String, Object> result = Maps.newHashMap();
@@ -55,9 +63,10 @@ public class OrganizationHierarchyController{
 		result.put("list", hierarchies);		
 		return result;
 	}
-	
+
+	@ApiOperation(value="新建或修改Hierarchy信息;新建时,ID请填0;修改时,ID请为要修改记录的ID",notes="注意： 参数params为必须项")
 	@PutMapping("/createOrUpdate")
-	public void createOrUpdateOrganizationHierarchy(@RequestBody OrganizationHierarchy params) throws Exception {
+	public void createOrUpdateOrganizationHierarchy(@ApiParam(name="params",value="要修改的层级数据",required=true) @RequestBody OrganizationHierarchy params) throws Exception {
 		try
 		{
 			hierarchy.createOrUpdateOrganizationHierarchy(params);
@@ -67,18 +76,21 @@ public class OrganizationHierarchyController{
 		}
 	}
 	
+	@ApiOperation(value="删除指定ID的组织层级",notes="注意： 参数id为必须项")
 	@DeleteMapping("/delete")
-	public int delete(@RequestParam("id") Long id)  throws Exception {
+	public int delete(@ApiParam(name="id",value="组织层级ID",required=true) @RequestParam("id") Long id)  throws Exception {
 		return hierarchy.deleteOrganizationHierarchy(id);
 	}
 	
+	@ApiOperation(value="批量删除组织层级",notes="注意： 参数idList为必须项")
 	@PostMapping("/batchDelete")
-	public void batchDeleteOrganizationHierarchy(@RequestBody BatchDeleteInput idList)  throws Exception {
+	public void batchDeleteOrganizationHierarchy(@ApiParam(name="idList",value="层级ID list",required=true) @RequestBody BatchDeleteInput idList)  throws Exception {
 		hierarchy.batchDeleteOrganizationHierarchy(idList);
 	}
 	
+	@ApiOperation(value="获得指定层级下可添加的下级层级列表",notes="注意： 参数curLevel为必须项")
 	@GetMapping("/getNextLevel")
-	public Map<String, Object> getNextLevel(@RequestParam("curLevel") Long curLevel) {
+	public Map<String, Object> getNextLevel(@ApiParam(name="curLevel",value="指定层级(非ID)",required=true) @RequestParam("curLevel") Long curLevel) {
 		Map<String, Object> result = Maps.newHashMap();
 		List<OrganizationHierarchy> hierarchies = hierarchy.getNextLevel(curLevel);
 		result.put("list", hierarchies);
