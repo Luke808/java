f164d6742e3cb610587eb8583defc79c68c5aaf6
haibo.liu <haibo.liu@accenture.com>
2018-11-23 11:51:27 +0800

    增加配置项

--- masterdata-starter/src/main/java/com/accenture/masterdata/starter/MasterdataApplication.java
+++ masterdata-starter/src/main/java/com/accenture/masterdata/starter/MasterdataApplication.java
@@ -2,6 +2,7 @@ package com.accenture.masterdata.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @SpringCloudApplication
 @Slf4j
+@EnableFeignClients(basePackages = {"com.accenture.smsf"})
 public class MasterdataApplication {
 	
     public static void main(String[] args) {
