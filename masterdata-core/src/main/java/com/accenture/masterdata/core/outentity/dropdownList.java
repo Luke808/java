031eae294272dfb87d377525e8e44f3e8f447e9b
Ding, Donglai <donglai.ding@accenture.com>
2018-12-20 16:28:48 +0800

    拆分masterdata和organization

--- masterdata-core/src/main/java/com/accenture/masterdata/core/outentity/dropdownList.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.masterdata.core.outentity;
-
-import lombok.Data;
-
-@Data
-public class dropdownList {
-	private String value;
-	private String name;
-}
--- masterdata-starter/src/main/java/com/accenture/masterdata/starter/Permissions.java
+++ masterdata-starter/src/main/java/com/accenture/masterdata/starter/Permissions.java
@@ -4,31 +4,47 @@ import com.accenture.smsf.authority.permission.loader.annotation.PermissionClass
 import com.accenture.smsf.authority.permission.loader.annotation.PermissionId;
 
 /**
- * @author lhb
- * @date 2018/7/20 11:29
+ * @author donglai,ding
+ * @date 2018/12/19 09:14
  * @since 1.0.0
  */
 @PermissionClass
 public class Permissions {
 	
-	// hierarchy权限设置
-    @PermissionId(value = "masterdata.hierarchy.view")
-    public static final String MASTERDATA_HIERARCHY_VIEW = "masterdata.hierarchy.view";
-    @PermissionId(value = "masterdata.hierarchy.manage.edit")
-    public static final String MASTERDATA_HIERARCHY_MANAGE_EDIT = "masterdata.hierarchy.manage.edit";
-    @PermissionId(value = "masterdata.hierarchy.manage.add")
-    public static final String MASTERDATA_HIERARCHY_MANAGE_ADD = "masterdata.hierarchy.manage.add";
-    @PermissionId(value = "masterdata.hierarchy.manage.delete")
-    public static final String MASTERDATA_HIERARCHY_MANAGE_DELETE = "masterdata.hierarchy.manage.delete";
-    
-    // organiztion权限设置
-    @PermissionId(value = "masterdata.organization.view")
-    public static final String MASTERDATA_ORGANIZATION_VIEW = "masterdata.organization.view";
-    @PermissionId(value = "masterdata.organization.manage.edit")
-    public static final String MASTERDATA_ORGANIZATION_MANAGE_EDIT = "masterdata.organization.manage.edit";
-    @PermissionId(value = "masterdata.organization.manage.add")
-    public static final String MASTERDATA_ORGANIZATION_MANAGE_ADD = "masterdata.organization.manage.add";
-    @PermissionId(value = "masterdata.organization.manage.delete")
-    public static final String MASTERDATA_ORGANIZATION_MANAGE_DELETE = "masterdata.organization.manage.delete";
+	// ClientServiceLevel权限设置
+    @PermissionId(value = "masterdata.client.service.level.view")
+    public static final String MASTERDATA_CLIENT_SERVICE_LEVEL_VIEW = "masterdata.client.service.level.view";
+    @PermissionId(value = "masterdata.client.service.level.manage")
+    public static final String MASTERDATA_CLIENT_SERVICE_LEVEL_MANAGE = "masterdata.client.service.level.manage";
+    // Process权限设置
+    @PermissionId(value = "masterdata.process.view")
+    public static final String MASTERDATA_PROCESS_VIEW = "masterdata.process.view";
+    @PermissionId(value = "masterdata.process.manage")
+    public static final String MASTERDATA_PROCESS_MANAGE = "masterdata.process.manage";
+    // Step权限设置
+    @PermissionId(value = "masterdata.step.view")
+    public static final String MASTERDATA_STEP_VIEW = "masterdata.step.view";
+    @PermissionId(value = "masterdata.step.manage")
+    public static final String MASTERDATA_STEP_MANAGE = "masterdata.step.manage";
+    // Tat权限设置
+    @PermissionId(value = "masterdata.tat.view")
+    public static final String MASTERDATA_TAT_VIEW = "masterdata.tat.view";
+    @PermissionId(value = "masterdata.tat.manage")
+    public static final String MASTERDATA_TAT_MANAGE = "masterdata.tat.manage";
+    // CutoffTime权限设置
+    @PermissionId(value = "masterdata.cutoff.time.view")
+    public static final String MASTERDATA_CUTOFF_TIME_VIEW = "masterdata.cutoff.time.view";
+    @PermissionId(value = "masterdata.cutoff.time.manage")
+    public static final String MASTERDATA_CUTOFF_TIME_MANAGE = "masterdata.cutoff.time.manage";
+    // Spt权限设置
+    @PermissionId(value = "masterdata.spt.view")
+    public static final String MASTERDATA_SPT_VIEW = "masterdata.spt.view";
+    @PermissionId(value = "masterdata.spt.manage")
+    public static final String MASTERDATA_SPT_MANAGE = "masterdata.spt.manage";
+    // CompanyServiceLevel权限设置
+    @PermissionId(value = "masterdata.company.service.level.view")
+    public static final String MASTERDATA_COMPANY_SERVICE_LEVEL_VIEW = "masterdata.company.service.level.view";
+    @PermissionId(value = "masterdata.company.service.level.manage")
+    public static final String MASTERDATA_COMPANY_SERVICE_LEVEL_MANAGE = "masterdata.company.service.level.manage";
     
 }
