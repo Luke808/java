419ee2c004ffb8f104468dd5e1df2fe2fbfe5caa
Ding, Donglai <donglai.ding@accenture.com>
2019-02-18 10:34:30 +0800

    UAT部署

--- smsf-masterdata.sql
+++ smsf-masterdata.sql
@@ -11,16 +11,16 @@ CREATE SCHEMA `smsf_master_data` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 --  PRIMARY KEY (`id`)
 -- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `t_tat`;
-CREATE TABLE `t_tat` (
+DROP TABLE IF EXISTS `smsf_master_data`.`t_tat`;
+CREATE TABLE `smsf_master_data`.`t_tat` (
   `id` varchar(50) NOT NULL,
   `process_id` varchar(50) DEFAULT NULL,
   `tat` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `t_client_service_level`;
-CREATE TABLE `t_client_service_level` (
+DROP TABLE IF EXISTS `smsf_master_data`.`t_client_service_level`;
+CREATE TABLE `smsf_master_data`.`t_client_service_level` (
   `id` varchar(50) NOT NULL,
   `code` varchar(255) DEFAULT NULL,
   `ety` decimal(65,0) DEFAULT NULL,
@@ -30,8 +30,8 @@ CREATE TABLE `t_client_service_level` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `t_company_service_level`;
-CREATE TABLE `t_company_service_level` (
+DROP TABLE IF EXISTS `smsf_master_data`.`t_company_service_level`;
+CREATE TABLE `smsf_master_data`.`t_company_service_level` (
   `id` varchar(50) NOT NULL,
   `parent_id` varchar(50) DEFAULT NULL,
   `code` varchar(255) DEFAULT NULL,
@@ -39,16 +39,16 @@ CREATE TABLE `t_company_service_level` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `t_cutoff_time`;
-CREATE TABLE `t_cutoff_time` (
+DROP TABLE IF EXISTS `smsf_master_data`.`t_cutoff_time`;
+CREATE TABLE `smsf_master_data`.`t_cutoff_time` (
   `id` varchar(50) NOT NULL,
   `process_id` varchar(50) DEFAULT NULL,
   `time` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `t_spt`;
-CREATE TABLE `t_spt` (
+DROP TABLE IF EXISTS `smsf_master_data`.`t_spt`;
+CREATE TABLE `smsf_master_data`.`t_spt` (
   `id` varchar(50) NOT NULL,
   `process_id` varchar(50) DEFAULT NULL,
   `step_id` varchar(50) DEFAULT NULL,
@@ -56,16 +56,16 @@ CREATE TABLE `t_spt` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `t_step`;
-CREATE TABLE `t_step` (
+DROP TABLE IF EXISTS `smsf_master_data`.`t_step`;
+CREATE TABLE `smsf_master_data`.`t_step` (
   `id` varchar(50) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `process_id` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `t_process`;
-CREATE TABLE `t_process` (
+DROP TABLE IF EXISTS `smsf_master_data`.`t_process`;
+CREATE TABLE `smsf_master_data`.`t_process` (
   `id` varchar(50) NOT NULL,
   `company_service_level_id` varchar(50) DEFAULT NULL,
   `name` varchar(200) DEFAULT NULL,
